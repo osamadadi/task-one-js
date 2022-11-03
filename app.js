b29df3b766bace4058974ba415d620465990ad1b
osamadadi <hafizmuhammadusama1@gmail.com>
2022-11-03 04:02:18 -0700

    now  ready to commit

---
+++ app.js
@@ -0,0 +1,114 @@
+window.alert("Thanks for your input")
+
+
+var name = "Mark";
+
+nationality = "U.S.";
+
+
+
+var lessonAuthor = "Mark";
+var guyWhoKeepsSayingHisOwnName = "Mark";
+var x = "Mark";
+
+
+
+
+var lastName = "Smith";
+var cityOfOrigin = "New Orleans";
+var aussieGreeting = "g'Day";
+
+
+
+
+
+// var abc  //legal
+// var 123  //illegal
+// var ab12 //legal
+// var 12ab //illegal
+// var var  //illegal
+// var @abc //illigal
+// var abc@ //illigal
+// var a+b //illegal
+// var a_b //lagal
+
+
+var ABC = "xyz"
+alert (ABC)
+
+
+var a = a
+a = "abc"
+console.log(a)
+
+
+var x = 20;
+var y = 5;
+z = x + y
+console.log(z)
+
+
+var a = 109
+var b = 76
+var c = 56
+d = a - b + c
+console.log(d)
+
+
+var a = 80
+var b = 40
+c = a *b 
+console.log(c)
+
+var a = 10 
+var b = 8
+c = a - b
+console.log(c)
+
+
+var x = 50
+var y = 90
+var z = 100
+ABC = x / y + z 
+console.log(ABC)
+
+
+
+var calculator = 100 % 4
+console.log(calculator)
+
+
+
+var java = 120 + 4
+console.log(java)
+
+
+
+var sim = 120 / 2
+console.log(sim)
+
+
+
+
+
+
+
+
+
+// a++;
+// ++a;
+// a--;
+// --a;
+// +-a;
+
+
+var num = 1;
+var a = num++;
+
+console.log(a)
+
+
+
+
+
+
